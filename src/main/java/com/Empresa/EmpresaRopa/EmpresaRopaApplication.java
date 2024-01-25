da39829452eace53d961f45957d8b60ec2a2b888
Alberto <alberto.fachan@gmail.com>
2024-01-25 18:29:18 +0100

    proyecto

---
+++ src/main/java/com/Empresa/EmpresaRopa/EmpresaRopaApplication.java
@@ -0,0 +1,15 @@
+package com.Empresa.EmpresaRopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaRopaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EmpresaRopaApplication.class, args);
+        System.out.println("Conexion hecha");
+	}
+
+}
---
+++ src/test/java/com/Empresa/EmpresaRopa/EmpresaRopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Empresa.EmpresaRopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaRopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
