f74cc5e71ba23250783a4576fb1f8323a2d54614
WebProba <alberto.fachan@gmail.com>
2024-01-29 15:32:32 +0100

    EN CASA

--- src/main/java/com/Empresa/EmpresaRopa/controlador/ControladorEmpleados.java
+++ src/main/java/com/Empresa/EmpresaRopa/controlador/ControladorEmpleados.java
@@ -1,6 +1,7 @@
 package com.Empresa.EmpresaRopa.controlador;
 
 import com.Empresa.EmpresaRopa.models.Employe;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,14 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@AllArgsConstructor
 public class ControladorEmpleados {
 
     private final RepositoryBuscarEmpleados repositoryBuscarEmpleados;
 
-    public ControladorEmpleados(RepositoryBuscarEmpleados operacionBuscar) {
-        this.repositoryBuscarEmpleados = operacionBuscar;
-
-    }
 
 
     @GetMapping("/empleados")
--- src/main/java/com/Empresa/EmpresaRopa/controlador/ControladorRopa.java
+++ src/main/java/com/Empresa/EmpresaRopa/controlador/ControladorRopa.java
@@ -1,8 +1,32 @@
 package com.Empresa.EmpresaRopa.controlador;
 
 
+import com.Empresa.EmpresaRopa.models.Employe;
+import com.Empresa.EmpresaRopa.models.Ropa;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class ControladorRopa {
+
+    private final RepositoryBuscarRopa repositoryBuscarRopa;
+
+
+    @GetMapping("/falda")
+    public Iterable<Ropa> findAllEmploye() {
+
+        return this.repositoryBuscarRopa.findAll();
+    }
+
+    @PostMapping("/falda")
+    public Ropa addOneRopa(@RequestBody new Ropa()) {
+
+        return this.repositoryBuscarRopa.save(new Ropa());
+    }
+
+
 }
