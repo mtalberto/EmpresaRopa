8e4d8491a21438e9ca408599a7550d0e3b3495ed
Alberto <alberto.fachan@gmail.com>
2024-02-05 17:41:43 +0100

    feito en clase

--- src/main/java/com/Empresa/empresaRopa/controlador/RepositoryBuscarCamiseta.java
+++ src/main/java/com/Empresa/empresaRopa/controlador/RepositoryBuscarCamiseta.java
@@ -1,9 +1,8 @@
 package com.Empresa.empresaRopa.controlador;
 
 import com.Empresa.empresaRopa.models.CamisetaEntity;
-
 import com.Empresa.empresaRopa.models.FaldaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
--- src/main/java/com/Empresa/empresaRopa/controlador/RepositoryBuscarEmpleados.java
+++ src/main/java/com/Empresa/empresaRopa/controlador/RepositoryBuscarEmpleados.java
@@ -1,15 +1,13 @@
 package com.Empresa.empresaRopa.controlador;
 
 import com.Empresa.empresaRopa.models.EmpleadoEntity;
-
-import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface RepositoryBuscarEmpleados extends JCrudRepository<EmpleadoEntity, Long > {
+public interface RepositoryBuscarEmpleados extends CrudRepository<EmpleadoEntity, Long > {
 //puedo hacer consultas personalizadas mediante  @Query("SELECT m FROM Modelo m WHERE m.atributo = :atributo")
 // @Query("SELECT m FROM Modelo m WHERE m.atributo = :atributo")
 //    List<Modelo> buscarPorAtributo(@Param("atributo") String atributo);
