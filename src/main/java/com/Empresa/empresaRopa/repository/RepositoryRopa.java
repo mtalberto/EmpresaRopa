1cf8458a937bd070146316de08e8f2ba86ee009a
Alberto <alberto.fachan@gmail.com>
2024-02-07 20:57:55 +0100

    feito en clase

--- src/main/java/com/Empresa/empresaRopa/repository/RepositoryRopa.java
+++
@@ -1,9 +0,0 @@
-package com.Empresa.empresaRopa.repository;
-
-import com.Empresa.empresaRopa.models.Ropa;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface RepositoryRopa extends CrudRepository {
-}
